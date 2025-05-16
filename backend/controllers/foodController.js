238c83f267085e35a3fad6c720bf47324e937cef
Bhargav Patel <A00303747@canadahanson.ca>
2025-05-16 09:56:55 -0400

    Backend is created for the  food items

---
+++ backend/controllers/foodController.js
@@ -0,0 +1,49 @@
+import foodModel from "../models/foodModel.js";
+import fs from 'fs'
+
+const addFood = async (req, res) => {
+    let image_filename = `${req.file?.filename}`;
+
+    const food = new foodModel({
+        name : req.body.name,
+        description : req.body.description,
+        price : req.body.price,
+        category : req.body.category,
+        image : image_filename
+    })
+
+    try {
+        await food.save()
+        res.json({success: true, message:"Food Added"})
+    } catch (error) {
+        console.log("Error while add in the food item",error)
+        res.json({success: false, message : "Errorƒ"})
+    }
+}
+
+// all food list
+const listFood = async ( req, res ) => {
+    try {
+        const foods = await foodModel.find({})
+        res.json({success : true , data : foods})
+    } catch (error) {
+        console.log("Error while getting the list of food.", error)
+        res.json({success : false, message:error})
+    }
+}
+
+// remove food item
+const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id)
+        fs.unlink(`uploads/${food.image}`, () => {})
+
+        await foodModel.findByIdAndDelete(req.body.id)
+        res.json({success : true , message : "Food Removed"})
+    } catch (error) {
+        console.log("Error while removing the food item" , error)
+        res.json({success : false, message : "Error"})
+    }
+}
+
+export {addFood, listFood,removeFood}
