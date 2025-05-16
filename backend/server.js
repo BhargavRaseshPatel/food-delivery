f7a0176d3410d2ffd02229514de8349546d26c8e
Bhargav Patel <A00303747@canadahanson.ca>
2025-05-16 15:48:06 -0400

    User login and register is created at backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import { connectDB } from './config/db.js'
 import foodRouter from './routes/foodRoute.js'
 import dotenv from 'dotenv'
+import userRouter from './routes/userRoute.js'
 
 // app config
 const app = express()
@@ -19,6 +20,7 @@ connectDB()
 // api endpoints
 app.use("/api/food", foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
 
 app.get('/', (req, res) => {
     res.send("API working")
